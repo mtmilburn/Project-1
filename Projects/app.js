1d4dca96c311c6897bc990fb68f56b06f6d80587
mattmilburn <matthew.t.milburn@gmail.com>
2023-12-20 14:06:57 -0500

    finally got the picturs to fit in the frames on projects

---
+++ Projects/app.js
@@ -0,0 +1,49 @@
+const images = document.querySelectorAll(".carousel-images .images");
+console.log(images)
+let currentImgIndex = 0
+/* Next button logic
+-------------------------------------------------------------- */
+let next = document.getElementById("nextbtn")
+next.addEventListener('click', () => {
+    // The image being displayed currently will now become the previous image
+    previousImgIndex = currentImgIndex
+    // The next image in the carousel will now become the current image
+    currentImgIndex++
+    console.log(currentImgIndex)
+
+    // Loop back to the beginning when on the last image
+    if (currentImgIndex >= images.length) {
+        console.log('Too high of an index! Going to reset to 0!')
+        currentImgIndex = 0;
+    }
+
+    // Hide the current image (which is now the previous image)
+    
+    images[previousImgIndex].style.display = 'none'
+    // Display the next image (which is now the current image)
+    images[currentImgIndex].style.display = 'block'
+})
+
+
+
+/* Prev button logic
+-------------------------------------------------------------- */
+let prev = document.getElementById("prevbtn")
+prev.addEventListener('click', () => {
+    // The image being displayed currently will now become the previous image
+    previousImgIndex = currentImgIndex
+    // The previous image in the carousel will now become the current image
+    currentImgIndex--
+    console.log(currentImgIndex)
+
+    // Loop back to the beginning when on the last image
+    if (currentImgIndex < 0) {
+        console.log('Too low of an index! Going to reset to 4!')
+        currentImgIndex = images.length - 1;
+    }
+
+    // Hide the current image (which is now the previous image)
+    images[previousImgIndex].style.display = 'none'
+    // Display the next image (which is now the current image)
+    images[currentImgIndex].style.display = 'block'
+})

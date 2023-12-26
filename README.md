2aec18cb5ebc807333fc4c420cff8584affdcd18
mattmilburn <matthew.t.milburn@gmail.com>
2023-12-26 14:37:07 -0500

    finished game, with errors

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Project-1
+
+
+### User Stories
+I'm a friend and fellow software engineer and want to see what Matt has put together. I'll go through the portfolio, play the games and read the literature. If need be I'll critque his work and give some pointers.
+
+I'm a hiring manager looking to sample what a prospective new employee has to ffer and what his strengths are. 
+
+I'm a family or friend with absolutely no experience in software and want to simply view this website and support my friend and family member.
+
+### Next Steps
+* upgrade the nav bar to be more attractive
+* give new features to the main menu including models, and hover features
+* add more projects and polish existing projects
+
+
+### Installation Instructions
+### Embedded Screenshot
+### _screenshots to let users know what to expect_?
+### Unsoled problems/Major Hurdles I had to overcome
+### A link to the project?
+### Technologies used
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Markdown](https://img.shields.io/badge/markdown-%23000000.svg?style=for-the-badge&logo=markdown&logoColor=white)
---
+++ Tic-Tac-Toe/app.js
@@ -0,0 +1,115 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~Constants~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [2, 5, 8],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~`State` Variables~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let board, turn, winner, tie
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~Cached Element References~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const squareElements = document.querySelectorAll('.sqr');
+const messageEl = document.querySelector('#message');
+// const sqrClick = document.querySelector('.sqr', );
+const reset = document.getElementById('reset');
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~Event Listeners~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+squareElements.forEach(function(sqr){
+    sqr.addEventListener('click', handleClick)
+})
+
+reset.addEventListener('click', init)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+init();
+
+//player starts game, board layout
+function init (){
+    board = [null, null, null, null, null, null, null, null, null];
+    turn = 1;
+    winner = false;
+    tie = false;
+    render();
+}
+
+function render(){
+    updateBoard();
+    updateMessage();
+}
+
+//the status of the board during game play, 1 is the player 'X', -1 is computer 'O', empty string is when board is reset
+function updateBoard(){
+board.forEach(function(sqrVal, idx){
+    if (board[idx] === 1){
+        squareElements[idx].textContent = 'X'
+        squareElements[idx].style.backgroundColor = 'lightgrey'
+    } else if (board[idx] === -1){
+        squareElements[idx].textContent = 'O'
+        squareElements[idx].style.backgroundColor = 'lightcoral'
+    } else {
+        squareElements[idx].textContent = ''
+    }
+})
+}
+
+//after each turn what happens
+function updateMessage() {
+    if (winner === false && tie === false){
+        messageEl.textContent = "It's the next players turn"
+    } else if (winner === false && tie === true){
+        messageEl.textContent = "It's a tie 😑!"
+    } else {
+        messageEl.textContent = "Congratulations! You won!🤩"
+    }
+}
+
+//update board array each turn
+function handleClick(evt) {
+    const sqIdx = evt.target.id[2]
+    if (board[sqIdx] || winner) return
+    placePiece(sqIdx)
+    checkForTie()
+    checkForWinner()
+    switchTurns()
+    render()
+}
+
+//changes the state of the board when you click box
+function placePiece(idx) {
+    board[idx] = turn
+}
+
+function checkForTie() {
+    if(board.includes(null)) {
+        tie = false
+    } else {
+        tie = true
+    }
+}
+
+function checkForWinner() {
+    if(
+        Math.abs(board[0] + board[1] + board[2]) === 3 ||
+        Math.abs(board[3] + board[4] + board[5]) === 3 ||
+        Math.abs(board[6] + board[7] + board[8]) === 3 ||
+        Math.abs(board[0] + board[3] + board[6]) === 3 ||
+        Math.abs(board[1] + board[4] + board[7]) === 3 ||
+        Math.abs(board[2] + board[5] + board[8]) === 3 ||
+        Math.abs(board[2] + board[4] + board[6]) === 3 ||
+        Math.abs(board[0] + board[4] + board[8]) === 3 
+    ) {
+        winner = true
+    }
+}
+
+function switchTurns(){
+    if (winner) return
+    turn *= -1
+}
